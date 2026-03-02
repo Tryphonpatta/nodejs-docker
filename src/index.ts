9be86f294c1b44cf089e0b0eccf38fc4b93f3a16
patthad <beebobbob956@gmai.com>
2026-03-03 00:01:23 +0700

    feat: 🎸 graceful shutdown

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,18 @@ app.get("/health", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Running on ${PORT}`);
 });
+
+[
+  `exit`,
+  `SIGQUIT`,
+  `SIGINT`,
+  `SIGUSR1`,
+  `SIGUSR2`,
+  `uncaughtException`,
+  `SIGTERM`,
+].forEach(function (event) {
+  process.on(event, function () {
+    console.log(`${event}, exiting...`);
+    process.exit(0);
+  });
+});
